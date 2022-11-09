af4b63b80854dfe6a0d2be7f7e6d4d76ccb09300
Twaha Mukammel <t.mukammel@gmail.com>
2022-11-09 18:05:06 +0600

    FIX: db config import issue

--- src/db/config/config.js
+++ src/db/config/config.js
@@ -1,4 +1,15 @@
-import { DB_CONFIG } from '../../configs';
+// Important for loading environment variables
+// when running migrations and seeders
+require('dotenv-flow').config({ silent: true });
+
+const DB_CONFIG = Object.freeze({
+	DB_USERNAME: process.env.DB_USERNAME,
+	DB_PASSWORD: process.env.DB_PASSWORD,
+	DB_DATABASE: process.env.DB_DATABASE,
+	DB_HOST: process.env.DB_HOST ?? '127.0.0.1',
+	DB_PORT: process.env.DB_PORT ?? '3306',
+	DB_CONNECTION: process.env.DB_CONNECTION ?? 'mysql'
+});
 
 module.exports = {
 	development: {
